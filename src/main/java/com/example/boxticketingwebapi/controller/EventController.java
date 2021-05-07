4de21251aab2bd6ee822bf11b57d0328f02049cb
Rima-ag <rimaalghossein@gmail.com>
2021-05-07 11:16:13 +0300

    Adds ResponseBody and ResponseStatus annotations

--- src/main/java/com/example/boxticketingwebapi/controller/EventController.java
+++ src/main/java/com/example/boxticketingwebapi/controller/EventController.java
@@ -3,8 +3,11 @@ package com.example.boxticketingwebapi.controller;
 import com.example.boxticketingwebapi.model.EventModel;
 import com.example.boxticketingwebapi.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +19,8 @@ public class EventController {
     private EventService eventService;
 
     @GetMapping(value = "/events", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<EventModel> getEvents() {
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody /*Inserts returned value in reponse body*/ List<EventModel> getEvents() {
         return this.eventService.getAllEvents();
     }
 
