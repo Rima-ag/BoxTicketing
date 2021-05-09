666c19203179f8da025b36340eb5af90b1ef1d20
Rima-ag <rimaalghossein@gmail.com>
2021-05-09 20:45:58 +0300

    Updates request url

--- src/main/java/com/example/boxticketingwebapi/controller/EventController.java
+++ src/main/java/com/example/boxticketingwebapi/controller/EventController.java
@@ -5,20 +5,18 @@ import com.example.boxticketingwebapi.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/events")
 public class EventController {
 
     @Autowired
     private EventService eventService;
 
-    @GetMapping(value = "/events", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody /*Inserts returned value in reponse body*/ List<EventModel> getEvents() {
         return this.eventService.getAllEvents();
